9c3961cd0ad828ed943854aba8f3370ce8fac16f
halubouski1 <halubouski2@gmail.com>
2026-04-24 20:36:23 +0300

    added yandex metrica

--- coookieconsent-init.js
+++ coookieconsent-init.js
@@ -21,7 +21,8 @@ window.addEventListener('load', function () {
         autoClear: {
           cookies: [
             { name: /^_ga/ },
-            { name: '_gid' }
+            { name: '_gid' },
+            { name: /^_ym_/ }
           ]
         }
       }
@@ -47,15 +48,52 @@ window.addEventListener('load', function () {
   window.dataLayer = window.dataLayer || [];
   function gtag() { dataLayer.push(arguments); }
 
-  if (CookieConsent.acceptedCategory('analytics')) {
+  let analyticsStarted = false;
+  function enableAnalytics() {
+    if (analyticsStarted) return;
+    analyticsStarted = true;
+
+    var gaSrc = 'https://www.googletagmanager.com/gtag/js?id=G-EDBSSXJLQ7';
+    var alreadyLoaded = false;
+    for (var s = 0; s < document.scripts.length; s++) {
+      if (document.scripts[s].src === gaSrc) { alreadyLoaded = true; break; }
+    }
+    if (!alreadyLoaded) {
+      var gaScript = document.createElement('script');
+      gaScript.async = true;
+      gaScript.src = gaSrc;
+      document.head.appendChild(gaScript);
+    }
+
     gtag('js', new Date());
     gtag('config', 'G-EDBSSXJLQ7');
+
+    (function (m, e, t, r, i, k, a) {
+      m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments); };
+      m[i].l = 1 * new Date();
+      for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+      k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a);
+    })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=108750640', 'ym');
+
+    ym(108750640, 'init', {
+      ssr: true,
+      webvisor: true,
+      clickmap: true,
+      ecommerce: "dataLayer",
+      referrer: document.referrer,
+      url: location.href,
+      accurateTrackBounce: true,
+      trackLinks: true
+    });
+  }
+
+  if (CookieConsent.acceptedCategory('analytics')) {
+    enableAnalytics();
   }
 
   window.addEventListener('cc:onConsent', function () {
     if (CookieConsent.acceptedCategory('analytics')) {
-      gtag('js', new Date());
-      gtag('config', 'G-EDBSSXJLQ7');
+      enableAnalytics();
     }
   });
 
